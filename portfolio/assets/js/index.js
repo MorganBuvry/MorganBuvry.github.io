0fb943ea0a2b6a1b8bdf8a9320dec5c001bc45e2
Morgan Buvry <morgan.buvry@laposte.net>
2023-06-07 22:55:29 +0200

    Refonte totale de mon portfolio, plus à mon image, les projets JS s'ajouteront petit à petit

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -1,38 +1,20 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  tablet: { smooth: true },
-  smartphone: { smooth: true },
+const inputs = document.querySelectorAll("input");
+const htmlcssContent = document.querySelector(".html-css");
+const javascriptContent = document.querySelector(".javascript");
+
+inputs.forEach((input) => {
+  input.addEventListener("click", (e) => {
+    if (e.target.id == "html-css") {
+      javascriptContent.style.display = "none";
+      htmlcssContent.style.display = "flex";
+    } else if (e.target.id == "javascript") {
+      htmlcssContent.style.display = "none";
+      javascriptContent.style.display = "block";
+      javascriptContent.classList.add("checked");
+    } else {
+      javascriptContent.classList.remove("checked");
+      javascriptContent.style.display = "block";
+      htmlcssContent.style.display = "flex";
+    }
+  });
 });
-
-// Body color
-
-scroll.on("scroll", () => {
-  if (document.querySelector("#color.is-inview")) {
-    document.body.style.background = "#000101";
-    document.body.style.color = "#fefeec";
-  } else {
-    document.body.style.background = "#fefeec";
-    document.body.style.color = "#000101";
-  }
-});
-
-// Scroll update all pages
-
-document.addEventListener("DOMContentLoaded", function () {
-  function ScrollUpdateDelay() {
-    setTimeout(function () {
-      scroll.update();
-    }, 500);
-  }
-
-  ScrollUpdateDelay();
-});
-
-// Scroll update trigger
-
-function ScrollUpdateTrig() {
-  setTimeout(function () {
-    scroll.update();
-  }, 500);
-}
