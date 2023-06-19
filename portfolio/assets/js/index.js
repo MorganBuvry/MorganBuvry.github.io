50caabc8f549d2339880dfa2ade0c23397db5b3e
Morgan Buvry <morgan.buvry@laposte.net>
2023-06-19 16:16:06 +0200

    Refonte totale de mon portfolio

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -1,20 +1,49 @@
-const inputs = document.querySelectorAll("input");
-const htmlcssContent = document.querySelector(".html-css");
-const javascriptContent = document.querySelector(".javascript");
+/* Affichage de l'heure en temps réel */
 
-inputs.forEach((input) => {
-  input.addEventListener("click", (e) => {
-    if (e.target.id == "html-css") {
-      javascriptContent.style.display = "none";
-      htmlcssContent.style.display = "flex";
-    } else if (e.target.id == "javascript") {
-      htmlcssContent.style.display = "none";
-      javascriptContent.style.display = "block";
-      javascriptContent.classList.add("checked");
-    } else {
-      javascriptContent.classList.remove("checked");
-      javascriptContent.style.display = "block";
-      htmlcssContent.style.display = "flex";
-    }
+function startTime() {
+  const today = new Date();
+  const hour = today.getHours();
+  let minutes = today.getMinutes();
+  minutes = checkTime(minutes);
+  document.querySelector(
+    ".nav-time"
+  ).innerHTML = ` <p>${hour} h ${minutes}</p>`;
+
+  let t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+startTime();
+
+/* Menu click */
+const menu = document.querySelector(".menu");
+const menuSpan = document.querySelectorAll(".menu-link span");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click", menuOpen);
+closeBtn.addEventListener("click", menuClose);
+navOverlay.addEventListener("click", menuClose);
+
+function menuOpen() {
+  menu.style.transform = "translateY(0)";
+  navOverlay.style.transform = "translateY(0)";
+  menuSpan.forEach((span) => {
+    span.style.transform = "translateY(0)";
   });
-});
+  nav.style.transform = "translate(0,150px)";
+}
+
+function menuClose() {
+  menu.style.transform = "translateY(120%)";
+  navOverlay.style.transform = "translateY(120%)";
+  menuSpan.forEach((span) => {
+    span.style.transform = "translateY(150%)";
+  });
+  nav.style.transform = "translate(0)";
+}
